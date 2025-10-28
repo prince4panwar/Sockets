c6b64fa58f365b95cf3700932aa5a3e9fde46130
Prince Panwar <princepanwar420420@gmail.com>
2025-10-28 17:16:31 +0100

    Added database to store chats of a room

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
+const connect = require("./config/database");
+const Chat = require("./models/Chat");
 
 const port = 3000;
 
@@ -9,17 +11,38 @@ const server = createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("join_room", (data) => {
+    socket.join(data.roomId);
+  });
 
-  socket.on("msg_send", (data) => {
-    io.emit("msg_receive", data);
+  socket.on("msg_send", async (data) => {
+    // io.emit("msg_receive", data);
     // socket.emit("msg_receive", data);
     // socket.broadcast.emit("msg_receive", data);
+
+    const chat = await Chat.create({
+      roomId: data.roomId,
+      userId: data.username,
+      content: data.msg,
+    });
+    io.to(data.roomId).emit("msg_receive", data);
   });
 });
 
 app.use("/", express.static(__dirname + "/public"));
 
-server.listen(port, () => {
+app.get("/chat/:roomId", async (req, res) => {
+  const chats = await Chat.find({ roomId: req.params.roomId }).select(
+    "content userId"
+  );
+  res.render("index.ejs", {
+    id: req.params.roomId,
+    chats: chats,
+  });
+});
+
+server.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
+  await connect();
+  console.log("mongo db connected");
 });
